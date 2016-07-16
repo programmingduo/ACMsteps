a03e2e14a94f7a6f0332251f9ff8eb5cec979bb2
programmingduo <wuduotju@163.com>
2016-07-16 12:11:00 +0800

    机房电脑

---
+++ 图论/二分图/hdu 2063.cpp
@@ -0,0 +1,14 @@
+#include<cstdio>
+
+int main ()
+{
+    int k, n, m;
+    while(~scanf("%d", &k) && k)
+    {
+        scanf("%d %d", &n, &m);
+        for(int i = 0; i < k; i ++)
+        {
+            scanf
+        }
+    }
+}
--- 构造，博弈/cf 226D.cpp
+++ 构造，博弈/cf 226D.cpp
@@ -9,7 +9,7 @@ int main ()
     int r, c;
     while(~scanf("%d %d", &r, &c))
     {
-        int flag1 = 0, flag2 = 0;
+        int flag1 = 0, flag2 = 0, flag;
         memset(sumr, 0, sizeof(sumr));
         memset(sumc, 0, sizeof(sumc));
         for(int i = 0; i < r; i ++)
@@ -21,8 +21,10 @@ int main ()
                 sumc[j] += a[i][j];
                 if(sumr[i] < 0)
                     flag1 ++;
-                if(sumc[i] < 0)
+                if(sumc[j] < 0)
                     flag2 ++;
+                if(sumr[i] < 0 || sumc[j] < 0)
+                    flag ++;
             }
         }
         if(!flag1 && !flag2)
@@ -30,22 +32,24 @@ int main ()
             printf("0\n0\n");
             continue;
         }
-
+        int t;
         memset(ror, 0, sizeof(ror));
         memset(roc, 0, sizeof(roc));
-        while(flag1 || flag2)
+        flag = 1;
+        while(flag)
         {
+            flag = 0;
             for(int i = 0; i < r; i ++)
             {
                 if(sumr[i] < 0)
                 {
+                    flag = 1;
                     sumr[i] = -sumr[i];
                     for(int j = 0; j < c; j ++)
                     {
                         a[i][j] = -a[i][j];
+                        t = sumc[j];
                         sumc[j] += a[i][j] *( 2);
-                        if(sumc[j] < 0)
-                            flag2 ++;
                     }
                     ror[i] ++;
                 }
@@ -55,19 +59,20 @@ int main ()
             {
                 if(sumc[i] < 0)
                 {
+                    flag = 1;
                     sumc[i] = -sumc[i];
-                    for(int j = 0; j < c; j ++)
+                    for(int j = 0; j < r; j ++)
                     {
-                        a[i][j] = -a[i][j];
-                        sumr[j] += a[i][j] * 2;
-                        if(sumr[j] < 0)
-                            flag1 ++;
+                        a[j][i] = -a[j][i];
+                        t = sumr[j];
+                        sumr[j] += a[j][i] * 2;
                     }
                     roc[i] ++;
                 }
                 flag2 = 0;
             }
         }
+
         int ans = 0;
         for(int i =0; i < r; i ++)
         {
@@ -77,7 +82,7 @@ int main ()
         printf("%d", ans);
         for(int i = 0; i < r; i ++)
             if(ror[i] % 2)
-            printf(" %d", i +1);
+            printf(" %d", i +1 );
         printf("\n");
 
         ans = 0;
@@ -89,7 +94,7 @@ int main ()
         printf("%d", ans);
         for(int i = 0; i < c; i ++)
             if(roc[i] % 2)
-            printf(" %d", i +1);
+            printf(" %d", i + 1);
         printf("\n");
     }
 }
